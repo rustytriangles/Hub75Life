d0cf72006856ec8cceb808a8fbcbcb3ccc5d0f94
rustytriangles <mike@rustytriangles.com>
2022-11-07 16:52:11 -0500

    Clean up cut&paste makefile mess

--- cmake/local.cmake
+++ cmake/local.cmake
@@ -1,1 +1,5 @@
 message(STATUS "found local.cmake")
+
+add_executable(TestInstantiator TestInstantiator.cxx)
+add_test(NAME TestInstantiator
+         COMMAND TestInstantiator)
--- cmake/pico.cmake
+++ cmake/pico.cmake
@@ -1,5 +1,18 @@
 message("found pico.cmake")
 
-add_executable(TestInstantiator TestInstantiator.cxx)
-add_test(NAME TestInstantiator
-         COMMAND TestInstantiator)
+# Include the Pico SDK cmake file
+# Include the subsidiary .cmake file to get the SDK
+list(APPEND CMAKE_MODULE_PATH "/Users/mike/development/pico-sdk/external")
+include(pico_sdk_import)
+
+# Link the Project to a source file (step 4.6)
+add_executable(${OUTPUT_NAME} hub75life.cpp)
+
+# Link the Project to an extra library (pico_stdlib)
+target_link_libraries(${OUTPUT_NAME}
+    pico_stdlib
+    pico_multicore
+)
+
+# Initalise the SDK
+pico_sdk_init()
