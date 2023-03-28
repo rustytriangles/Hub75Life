ae4c1680ad4e45941d725a1cc4b8b415f3a0fbf1
rustytriangles <mike@rustytriangles.com>
2023-03-28 15:59:17 -0400

    Pull in DMA code from pimoroni library

--- cmake/pico.cmake
+++ cmake/pico.cmake
@@ -13,15 +13,19 @@ include(pico_sdk_import)
 set(PICO_CXX_ENABLE_EXCEPTIONS 1)
 
 # Link the Project to a source file (step 4.6)
-add_executable(${OUTPUT_NAME} src/hub75life.cpp src/pixel.cpp src/bitset.cpp src/life.cpp)
+add_executable(${OUTPUT_NAME} src/hub75life.cpp src/bitset.cpp src/life.cpp src/util.cpp)
+target_compile_options(${OUTPUT_NAME} PRIVATE -O3)
 
 # Link the Project to an extra library (pico_stdlib)
 target_link_libraries(${OUTPUT_NAME}
     pico_stdlib
     pico_multicore
+    hub75
 )
 
 # Initalise the SDK
 pico_sdk_init()
 
 pico_add_extra_outputs(${OUTPUT_NAME})
+pico_enable_stdio_usb(${OUTPUT_NAME} 1)
+pico_enable_stdio_uart(${OUTPUT_NAME} 0)
--- src/hub75life.cpp
+++ src/hub75life.cpp
@@ -4,62 +4,38 @@
 
 #include "bitset.hpp"
 #include "hub75life.hpp"
-#include "hub75util.hpp"
 #include "life.hpp"
 #include "util.hpp"
 
+#include "hub75.hpp"
+
 volatile bool flip = false;
-
-// Create our front and back buffers.
-// We'll draw into the frontbuffer and then copy everything into the backbuffer which will be used to refresh the screen.
-// Double-buffering the display avoids screen tearing with fast animations or slow update rates.
-Pixel backbuffer[FB_WIDTH][FB_HEIGHT];
-Pixel frontbuffer[FB_WIDTH][FB_HEIGHT];
-
-void clear_buffer(Pixel (&buffer)[FB_WIDTH][FB_HEIGHT], const Pixel& color) {
-    for(auto x = 0u; x < FB_HEIGHT; x++) {
-        for(auto y = 0u; y < FB_HEIGHT; y++) {
-            buffer[x][y] = color;
-        }
-    }
-}
 
 const uint32_t GRID_WIDTH = 128;
 const uint32_t GRID_HEIGHT = 64;
 
-void set_pixels(Pixel (&buffer)[FB_WIDTH][FB_HEIGHT], BitSet &mask, Pixel &curr_color) {
+Hub75 hub75(GRID_WIDTH, GRID_HEIGHT, nullptr, PANEL_GENERIC, true);
+
+void set_pixels(BitSet &mask, Pixel &curr_color) {
     for(auto x = 0u; x < (uint8_t)std::min((uint32_t)FB_WIDTH,GRID_WIDTH); x++) {
         for(auto y = 0u; y < FB_HEIGHT; y++) {
             if (mask.test(x,y)) {
-                buffer[x][y] = curr_color;
+                hub75.set_color(x,y,curr_color);
             }
         }
     }
 }
 
-void hub75_flip () {
-    flip = true; // TODO: rewrite to semaphore
-}
-
-void hub75_display_update() {
-
-    // Ridiculous register write nonsense for the FM6126A-based 64x64 matrix
-    FM6126A_write_register(0b1111111111111110, 12);
-    FM6126A_write_register(0b0000001000000000, 13);
-
-    while (true) {
-        // 0. Copy the contents of the front buffer into our backbuffer for output to the display.
-        //    This uses another whole backbuffer worth of memory, but prevents visual tearing at low frequencies.
-        if (flip) {
-            memcpy((uint8_t *)backbuffer, (uint8_t *)frontbuffer, FB_WIDTH * FB_HEIGHT * sizeof(Pixel));
-            flip = false;
-        }
-
-        push_buffer(&backbuffer[0][0], FB_WIDTH, FB_HEIGHT);
-    }
+void __isr dma_complete() {
+    hub75.dma_complete();
 }
 
 int main() {
+
+    stdio_init_all();
+
+    hub75.start(dma_complete);
+
     // 1.3v allows overclock to ~280000-300000 but YMMV. Faster clock = faster screen update rate!
     // vreg_set_voltage(VREG_VOLTAGE_1_30);
     // sleep_ms(100);
@@ -67,11 +43,6 @@ int main() {
     // 200MHz is roughly about the lower limit for driving a 64x64 display smoothly.
     // Just don't look at it out of the corner of your eye.
     set_sys_clock_khz(200000, false);
-
-    init_pins();
-
-    // Launch the display update routine on Core 1, it's hungry for cycles!
-    multicore_launch_core1(hub75_display_update);
 
     rand_init();
 
@@ -85,6 +56,8 @@ int main() {
 
     srand(time(NULL));
     int countdown = 0;
+    constexpr int num_gen = 5000;
+    absolute_time_t prev_time = {};
 
     while (true) {
         nextmask.clear();
@@ -92,6 +65,14 @@ int main() {
         Pixel curr_color = hsv_to_rgb(curr_hue, 1.f, 1.f);
 
         if (countdown == 0) {
+            absolute_time_t curr_time = get_absolute_time();
+
+            int64_t delta = absolute_time_diff_us(prev_time, curr_time);
+            prev_time = curr_time;
+            double sec = (double)delta/1.e6;
+            printf("Generated %d generations in %lf seconds (%lf)\n",
+                   num_gen, sec, (double)num_gen / sec);
+
             for (int x=0u; x<GRID_WIDTH; x++) {
                 for (int y=0u; y<GRID_HEIGHT; y++) {
                     if (random_between(0.f, 1.f) > 0.75f) {
@@ -100,21 +81,20 @@ int main() {
                 }
             }
 
-            clear_buffer(frontbuffer, background);
-            set_pixels(frontbuffer, prevmask, curr_color);
+            set_pixels(prevmask, curr_color);
 
-            countdown = 5000;
+            countdown = num_gen;
         }
         countdown -= 1;
 
         tick(nextmask, prevmask);
 
-        set_pixels(frontbuffer, nextmask, curr_color);
+        set_pixels(nextmask, curr_color);
 
         curr_hue = fmod(curr_hue + hue_step, 1.);
         prevmask = nextmask;
 
-        hub75_flip();
+        hub75.flip(true); // Flip and clear to the background colour
 
         sleep_ms(1);
     }
